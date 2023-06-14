e0bd639f0f47295dfae7b7c8bb3e65ed34192188
Tax0787 <93974004+Tax0787@users.noreply.github.com>
2023-06-15 08:18:51 +0900

    fix bug

--- TaxGitClone.py
+++ TaxGitClone.py
@@ -2,8 +2,6 @@ o=open
 with o('temp.sh','w')as f:
     you=input("You'r Github NickName : ")
     repo=input("You'r repo name : ")
-    source=f"gi
-    
-    qt clone https://github.com/{you}/{repo}.git"
+    source=f"git clone https://github.com/{you}/{repo}.git"
     input(f'sourse is : "{source}" > ')
     f.write(source)
